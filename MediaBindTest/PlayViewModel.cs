ef80f0672ddfe579f01cdd3cc58d9a54ede2a538
v-zhumin <v-zhumin@V-ZHUMIN-W10>
2017-03-20 17:57:21 +0800

    Add project files.

---
+++ MediaBindTest/PlayViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace MediaBindTest
+{
+    public class PlayViewModel
+    {
+        public ICommand OnPlayButton { get; set; }
+        private MediaElement _mediaElement;
+
+        public PlayViewModel()
+        {
+            OnPlayButton = new CommandHandler<MediaElement>((o) => { Play(o); });
+        }
+
+        public void Play(object obj)
+        {
+            try
+            {
+                _mediaElement = obj as MediaElement;
+                _mediaElement.Source = new Uri("ms-appx:///Assets/hello.mp3");
+                _mediaElement.Play();
+            }
+            catch (Exception exception)
+            {
+            }
+        }
+    }
+
+    public class CommandHandler<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action<T> _action;
+
+        public CommandHandler(Action<T> action)
+        {
+            this._action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._action((T)parameter);
+        }
+    }
+}
